79b459856806357fbe3f840e8ad799d3db6dd615
dru <dru@dru-laptop.(none)>
2010-07-23 20:08:14 -0700

    working javascript code for google chart

--- rrd_google_chart.rb
+++ rrd_google_chart.rb
@@ -3,7 +3,7 @@ class RRDGoogleChart
     def initialize
         @plot_data = {}
         @columns = []
-        @column_count = 0
+        @row_count = 0
         @data_points = []
     end
     
@@ -12,7 +12,6 @@ class RRDGoogleChart
     end
     
     def graph
-        @column_count = @plot_data.keys.size
         series_count = 0
         @plot_data.each do |title, plot_data|
             series_count += 1
@@ -20,10 +19,11 @@ class RRDGoogleChart
             @columns << "data.addColumn('string', 'title#{series_count}');"
             @columns << "data.addColumn('string', 'text#{series_count}');"
             plot_data.each_index do |i|
+		@row_count += 1
                 data_point = plot_data[i]
                 time_value = Time.at(data_point.first.to_i)
                 data_value = data_point.last
-                @data_points << "data.setValue(#{i}, 0, new Date(#{time_value.year}, #{time_value.month.to_i() - 1} ,#{time_value.day}));"
+                @data_points << "data.setValue(#{i}, 0, new Date(#{time_value.year}, #{time_value.month.to_i() - 1} ,#{time_value.day}, #{time_value.hour}, #{time_value.min}, #{time_value.sec}));"
                 @data_points << "data.setValue(#{i}, #{(series_count==1)? 1 : 1 + (series_count-1)*3}, #{data_value});"
             end
         end
@@ -42,9 +42,10 @@ class RRDGoogleChart
         }]
         
         columns = @columns.join("\n")
+	rows = "data.addRows(#{@row_count});\n"
         data_points = @data_points.join("\n")
         
-        javascript % (columns + data_points)
+        javascript % (columns + rows + data_points)
     end
 end
 
@@ -53,6 +54,7 @@ if __FILE__ == $0
         require 'rrd_fetch.rb'
         data = rrd_fetch(ARGV[0])
         data.delete_at(0)
+        data = data.select{|d| not d[1].nan? }
         chart = RRDGoogleChart.new
         chart.add_plot_data('CPU', data)
         chart.graph
